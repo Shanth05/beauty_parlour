eb2bbe8070ba37a6fb252f6b8bfc990d0fbb6763
Subiramaniam Pirashanth <shanth.pirashanth5@gmail.com>
2025-04-12 23:01:41 +0530

    Update footer.php

--- components/footer.php
+++ components/footer.php
@@ -6,7 +6,7 @@
          <h3>Quick links</h3>
          <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
          <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
-         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="services.php"> <i class="fas fa-angle-right"></i> Services</a>
          <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
       </div>
 
@@ -14,8 +14,8 @@
          <h3>Extra links</h3>
          <a href="user_login.php"> <i class="fas fa-angle-right"></i> Login</a>
          <a href="user_register.php"> <i class="fas fa-angle-right"></i> Register</a>
-         <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
-         <a href="orders.php"> <i class="fas fa-angle-right"></i> Orders</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Booking Cart</a>
+         <a href="appointments.php"> <i class="fas fa-angle-right"></i> Appointments</a>
       </div>
 
       <div class="box">
