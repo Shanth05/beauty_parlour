e2b8b9c2becc6000321123bd43917dd9956a6423
Subiramaniam Pirashanth <shanth.pirashanth5@gmail.com>
2025-04-12 23:01:30 +0530

    Update products.php

--- admin/products.php
+++ admin/products.php
@@ -90,7 +90,7 @@ if(isset($_GET['delete'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Products</title>
+   <title>Services</title>
 
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 
@@ -103,16 +103,16 @@ if(isset($_GET['delete'])){
 
 <section class="add-products">
 
-   <h1 class="heading">Add Product</h1>
+   <h1 class="heading">Add Service</h1>
 
    <form action="" method="post" enctype="multipart/form-data">
       <div class="flex">
          <div class="inputBox">
-            <span>Product Name (required)</span>
+            <span>Service Name (required)</span>
             <input type="text" class="box" required maxlength="100" placeholder="enter product name" name="name">
          </div>
          <div class="inputBox">
-            <span>Product Price (required)</span>
+            <span>Service Price (required)</span>
             <input type="number" min="0" class="box" required max="9999999999" placeholder="enter product price" onkeypress="if(this.value.length == 10) return false;" name="price">
          </div>
         <div class="inputBox">
@@ -128,7 +128,7 @@ if(isset($_GET['delete'])){
             <input type="file" name="image_03" accept="image/jpg, image/jpeg, image/png, image/webp" class="box" required>
         </div>
          <div class="inputBox">
-            <span>Product description (required)</span>
+            <span>Service description (required)</span>
             <textarea name="details" placeholder="enter product details" class="box" required maxlength="500" cols="30" rows="10"></textarea>
          </div>
       </div>
@@ -140,7 +140,7 @@ if(isset($_GET['delete'])){
 
 <section class="show-products">
 
-   <h1 class="heading">Products Added.</h1>
+   <h1 class="heading">Services Added.</h1>
 
    <div class="box-container">
 
