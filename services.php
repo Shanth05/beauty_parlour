7e1b45738402aa255f3b2ea0cfd295b8b24c747d
Subiramaniam Pirashanth <shanth.pirashanth5@gmail.com>
2025-05-01 22:53:20 +0530

    Update services.php

--- services.php
+++ services.php
@@ -18,70 +18,45 @@ include 'components/wishlist_cart.php';
 <html lang="en">
 <head>
    <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Services</title>
-   
-   <!-- font awesome cdn link  -->
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-
-   <!-- custom css file link  -->
+   <title>Beauty Bliss | Services</title>
    <link rel="stylesheet" href="css/style.css">
-
 </head>
 <body>
-   
-<?php include 'components/user_header.php'; ?>
 
-<section class="products">
+<?php include 'header.php'; ?>
 
-   <h1 class="heading">Latest Services.</h1>
+<section class="services">
+   <h1 class="heading"> our <span>services</span> </h1>
 
    <div class="box-container">
 
-   <?php
-     $select_products = $conn->prepare("SELECT * FROM `products`"); 
-     $select_products->execute();
-     if($select_products->rowCount() > 0){
-      while($fetch_product = $select_products->fetch(PDO::FETCH_ASSOC)){
-   ?>
-   <form action="" method="post" class="box">
-      <input type="hidden" name="pid" value="<?= $fetch_product['id']; ?>">
-      <input type="hidden" name="name" value="<?= $fetch_product['name']; ?>">
-      <input type="hidden" name="price" value="<?= $fetch_product['price']; ?>">
-      <input type="hidden" name="image" value="<?= $fetch_product['image_01']; ?>">
-      <button class="fas fa-heart" type="submit" name="add_to_wishlist"></button>
-      <a href="quick_view.php?pid=<?= $fetch_product['id']; ?>" class="fas fa-eye"></a>
-      <img src="uploaded_img/<?= $fetch_product['image_01']; ?>" alt="">
-      <div class="name"><?= $fetch_product['name']; ?></div>
-      <div class="flex">
-         <div class="price"><span>Nrs.</span><?= $fetch_product['price']; ?><span>/-</span></div>
-         <input type="number" name="qty" class="qty" min="1" max="99" onkeypress="if(this.value.length == 2) return false;" value="1">
+      <div class="box">
+         <img src="images/haircut.png" alt="Haircut">
+         <h3>Hair Styling</h3>
+         <p>Trendy haircuts, styling, and blow-dry services by professionals.</p>
       </div>
-      <input type="submit" value="add to cart" class="btn" name="add_to_cart">
-   </form>
-   <?php
-      }
-   }else{
-      echo '<p class="empty">no products found!</p>';
-   }
-   ?>
+
+      <div class="box">
+         <img src="images/makeup.png" alt="Makeup">
+         <h3>Bridal & Party Makeup</h3>
+         <p>Perfect look for your big day with our bridal & party makeup services.</p>
+      </div>
+
+      <div class="box">
+         <img src="images/spa.png" alt="Spa">
+         <h3>Spa & Facial</h3>
+         <p>Relax with our luxury spa and facial treatment packages.</p>
+      </div>
+
+      <div class="box">
+         <img src="images/nails.png" alt="Nail Art">
+         <h3>Manicure & Pedicure</h3>
+         <p>Pamper your hands and feet with our nail care services.</p>
+      </div>
 
    </div>
-
 </section>
-
-
-
-
-
-
-
-
-
-
-
-
 
 <?php include 'components/footer.php'; ?>
 
