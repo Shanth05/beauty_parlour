df8ef33dfb6c55ee3150c73b0e82f4618f93cd5d
Subiramaniam Pirashanth <shanth.pirashanth5@gmail.com>
2025-05-12 21:48:55 +0530

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,149 +1,297 @@
 <?php
-
-include '../components/connect.php';
-
 session_start();
-
-$admin_id = $_SESSION['admin_id'];
-
-if(!isset($admin_id)){
-   header('location:admin_login.php');
-}
-
-?>
-
-<!DOCTYPE html>
-<html lang="en">
+error_reporting(0);
+include('includes/dbconnection.php');
+if (strlen($_SESSION['bpmsaid']==0)) {
+  header('location:logout.php');
+  } 
+     ?>
+<!DOCTYPE HTML>
+<html>
 <head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Dashboard</title>
+<title>BPMS | Admin Dashboard</title>
 
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- Bootstrap Core CSS -->
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<!-- Custom CSS -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<!-- font CSS -->
+<!-- font-awesome icons -->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!-- //font-awesome icons -->
+ <!-- js-->
+<script src="js/jquery-1.11.1.min.js"></script>
+<script src="js/modernizr.custom.js"></script>
+<!--webfonts-->
+<link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
+<!--//webfonts--> 
+<!--animate-->
+<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+<script src="js/wow.min.js"></script>
+	<script>
+		 new WOW().init();
+	</script>
+<!--//end-animate-->
+<!-- chart -->
+<script src="js/Chart.js"></script>
+<!-- //chart -->
+<!--Calender-->
+<link rel="stylesheet" href="css/clndr.css" type="text/css" />
+<script src="js/underscore-min.js" type="text/javascript"></script>
+<script src= "js/moment-2.2.1.js" type="text/javascript"></script>
+<script src="js/clndr.js" type="text/javascript"></script>
+<script src="js/site.js" type="text/javascript"></script>
+<!--End Calender-->
+<!-- Metis Menu -->
+<script src="js/metisMenu.min.js"></script>
+<script src="js/custom.js"></script>
+<link href="css/custom.css" rel="stylesheet">
+<!--//Metis Menu -->
+</head> 
+<body class="cbp-spmenu-push">
+<div class="main-content">
+		
+		 <?php include_once('includes/sidebar.php');?>
+		
+	<?php include_once('includes/header.php');?>
+		<!-- main content start-->
+		<div id="page-wrapper" class="row calender widget-shadow">
+			<div class="main-page">
+				
+			
+				<div class="row calender widget-shadow">
+					<div class="row-one">
+					<div class="col-md-4 widget">
+						<?php $query1=mysqli_query($con,"Select * from tbluser");
+$totalcust=mysqli_num_rows($query1);
+?>
+						<div class="stats-left ">
+							<h5>Total</h5>
+							<h4>Customer</h4>
+						</div>
+						<div class="stats-right">
+							<label> <?php echo $totalcust;?></label>
+						</div>
+						<div class="clearfix"> </div>	
+					</div>
+					<div class="col-md-4 widget states-mdl">
+						<?php $query2=mysqli_query($con,"Select * from tblbook");
+$totalappointment=mysqli_num_rows($query2);
+?>
+						<div class="stats-left">
+							<h5>Total</h5>
+							<h4>Appointment</h4>
+						</div>
+						<div class="stats-right">
+							<label> <?php echo $totalappointment;?></label>
+						</div>
+						<div class="clearfix"> </div>	
+					</div>
+					<div class="col-md-4 widget states-last">
+						<?php $query3=mysqli_query($con,"Select * from tblbook where Status='Selected'");
+$totalaccapt=mysqli_num_rows($query3);
+?>
+						<div class="stats-left">
+							<h5>Total</h5>
+							<h4>Accepted Apt</h4>
+						</div>
+						<div class="stats-right">
+							<label><?php echo $totalaccapt;?></label>
+						</div>
+						<div class="clearfix"> </div>	
+					</div>
+					<div class="clearfix"> </div>	
+				</div>
+						
+					</div>
 
-   <link rel="stylesheet" href="../css/admin_style.css">
+				<div class="row calender widget-shadow">
+					<div class="row-one">
+					<div class="col-md-4 widget">
+						<?php $query4=mysqli_query($con,"Select * from tblbook where Status='Rejected'");
+$totalrejapt=mysqli_num_rows($query4);
+?>
+						<div class="stats-left ">
+							<h5>Total</h5>
+							<h4>Rejected Apt</h4>
+						</div>
+						<div class="stats-right">
+							<label> <?php echo $totalrejapt;?></label>
+						</div>
+						<div class="clearfix"> </div>	
+					</div>
+					<div class="col-md-4 widget states-mdl">
+						<?php $query5=mysqli_query($con,"Select * from  tblservices");
+$totalser=mysqli_num_rows($query5);
+?>
+						<div class="stats-left">
+							<h5>Total</h5>
+							<h4>Services</h4>
+						</div>
+						<div class="stats-right">
+							<label> <?php echo $totalser;?></label>
+						</div>
+						<div class="clearfix"> </div>	
+					</div>
+					<div class="col-md-4 widget states-last">
+						<?php
+//todays sale
+ $query6=mysqli_query($con,"select tblinvoice.ServiceId as ServiceId, tblservices.Cost
+ from tblinvoice 
+  join tblservices  on tblservices.ID=tblinvoice.ServiceId where date(PostingDate)=CURDATE();");
+while($row=mysqli_fetch_array($query6))
+{
+$todays_sale=$row['Cost'];
+$todysale+=$todays_sale;
 
-</head>
-<body>
+}
+ ?>
+						<div class="stats-left">
+							<h5>Today</h5>
+							<h4>Sales</h4>
+						</div>
+						<div class="stats-right">
+							<label> <?php 
+if($todysale==""):
+							echo "0";
+else:
+	echo $todysale;
+endif;
+						?></label>
+						</div>
+						<div class="clearfix"> </div>	
+					</div>
+					<div class="clearfix"> </div>	
+				</div>
+						
+					</div>
 
-<?php include '../components/admin_header.php'; ?>
+				<div class="row calender widget-shadow">
+					<div class="row-one">
+					<div class="col-md-4 widget">
+						<?php
+//Yesterday's sale
+ $query7=mysqli_query($con,"select tblinvoice.ServiceId as ServiceId, tblservices.Cost
+ from tblinvoice 
+  join tblservices  on tblservices.ID=tblinvoice.ServiceId where date(PostingDate)=CURDATE()-1;");
+while($row7=mysqli_fetch_array($query7))
+{
+$yesterdays_sale=$row7['Cost'];
+$yesterdaysale+=$yesterdays_sale;
 
-<section class="dashboard">
+}
+ ?>
+						<div class="stats-left ">
+							<h5>Yesterday</h5>
+							<h4>Sales</h4>
+						</div>
+						<div class="stats-right">
+							<label> <?php 
+if($yesterdaysale==""):
+							echo "0";
+else:
+	echo $yesterdaysale;
+endif;
+						?></label>
+						</div>
+						<div class="clearfix"> </div>	
+					</div>
+					<div class="col-md-4 widget states-mdl">
+						<?php
+//Last Sevendays Sale
+ $query8=mysqli_query($con,"select tblinvoice.ServiceId as ServiceId, tblservices.Cost
+ from tblinvoice 
+  join tblservices  on tblservices.ID=tblinvoice.ServiceId where date(PostingDate)>=(DATE(NOW()) - INTERVAL 7 DAY);");
+while($row8=mysqli_fetch_array($query8))
+{
+$sevendays_sale=$row8['Cost'];
+$tseven+=$sevendays_sale;
 
-   <h1 class="heading">Dashboard</h1>
+}
+ ?>
+						<div class="stats-left">
+							<h5>Last Sevendays</h5>
+							<h4>Sale</h4>
+						</div>
+						<div class="stats-right">
+							<label> <?php 
 
-   <div class="box-container">
+						if($tseven==""):
+							echo "0";
+else:
+	echo $tseven;
+endif;?></label>
+						</div>
+						<div class="clearfix"> </div>	
+					</div>
+					<div class="col-md-4 widget states-last">
+						<?php
+//Total Sale
+ $query9=mysqli_query($con,"select tblinvoice.ServiceId as ServiceId, tblservices.Cost
+ from tblinvoice 
+  join tblservices  on tblservices.ID=tblinvoice.ServiceId");
+while($row9=mysqli_fetch_array($query9))
+{
+$total_sale=$row9['Cost'];
+$totalsale+=$total_sale;
 
-      <div class="box">
-         <h3>Welcome!</h3>
-         <p><?= $fetch_profile['name']; ?></p>
-         <a href="update_profile.php" class="btn">Update Profile</a>
-      </div>
+}
+ ?>
+						<div class="stats-left">
+							<h5>Total</h5>
+							<h4>Sales</h4>
+						</div>
+						<div class="stats-right">
+							<label><?php
 
-      <div class="box">
-         <?php
-            $total_pendings = 0;
-            $select_pendings = $conn->prepare("SELECT * FROM `orders` WHERE payment_status = ?");
-            $select_pendings->execute(['pending']);
-            if($select_pendings->rowCount() > 0){
-               while($fetch_pendings = $select_pendings->fetch(PDO::FETCH_ASSOC)){
-                  $total_pendings += $fetch_pendings['total_price'];
-               }
-            }
-         ?>
-         <h3><span>Nrs.</span><?= $total_pendings; ?><span>/-</span></h3>
-         <p>Total pendings</p>
-         <a href="placed_appointments.php" class="btn">See Appointments.</a>
-      </div>
+		if($totalsale==""):
+							echo "0";
+else:
+	echo $totalsale;
+endif;
+						?></label>
+						</div>
+						<div class="clearfix"> </div>	
+					</div>
+					<div class="clearfix"> </div>	
+				</div>
+						
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+		<!--footer-->
+		<?php include_once('includes/footer.php');?>
+        <!--//footer-->
+	</div>
+	<!-- Classie -->
+		<script src="js/classie.js"></script>
+		<script>
+			var menuLeft = document.getElementById( 'cbp-spmenu-s1' ),
+				showLeftPush = document.getElementById( 'showLeftPush' ),
+				body = document.body;
+				
+			showLeftPush.onclick = function() {
+				classie.toggle( this, 'active' );
+				classie.toggle( body, 'cbp-spmenu-push-toright' );
+				classie.toggle( menuLeft, 'cbp-spmenu-open' );
+				disableOther( 'showLeftPush' );
+			};
+			
 
-      <div class="box">
-         <?php
-            $total_completes = 0;
-            $select_completes = $conn->prepare("SELECT * FROM `orders` WHERE payment_status = ?");
-            $select_completes->execute(['completed']);
-            if($select_completes->rowCount() > 0){
-               while($fetch_completes = $select_completes->fetch(PDO::FETCH_ASSOC)){
-                  $total_completes += $fetch_completes['total_price'];
-               }
-            }
-         ?>
-         <h3><span>Nrs.</span><?= $total_completes; ?><span>/-</span></h3>
-         <p>Completed orders</p>
-         <a href="placed_appointments.php" class="btn">See orders</a>
-      </div>
-
-      <div class="box">
-         <?php
-            $select_orders = $conn->prepare("SELECT * FROM `orders`");
-            $select_orders->execute();
-            $number_of_orders = $select_orders->rowCount()
-         ?>
-         <h3><?= $number_of_orders; ?></h3>
-         <p>Appointments Placed.</p>
-         <a href="placed_appointments.php" class="btn">See orders.</a>
-      </div>
-
-      <div class="box">
-         <?php
-            $select_products = $conn->prepare("SELECT * FROM `products`");
-            $select_products->execute();
-            $number_of_products = $select_products->rowCount()
-         ?>
-         <h3><?= $number_of_products; ?></h3>
-         <p>Services added</p>
-         <a href="products.php" class="btn">See products</a>
-      </div>
-
-      <div class="box">
-         <?php
-            $select_users = $conn->prepare("SELECT * FROM `users`");
-            $select_users->execute();
-            $number_of_users = $select_users->rowCount()
-         ?>
-         <h3><?= $number_of_users; ?></h3>
-         <p>Normal users</p>
-         <a href="users_accounts.php" class="btn">See Users</a>
-      </div>
-
-      <div class="box">
-         <?php
-            $select_admins = $conn->prepare("SELECT * FROM `admins`");
-            $select_admins->execute();
-            $number_of_admins = $select_admins->rowCount()
-         ?>
-         <h3><?= $number_of_admins; ?></h3>
-         <p>Admin users</p>
-         <a href="admin_accounts.php" class="btn">See admins</a>
-      </div>
-
-      <div class="box">
-         <?php
-            $select_messages = $conn->prepare("SELECT * FROM `messages`");
-            $select_messages->execute();
-            $number_of_messages = $select_messages->rowCount()
-         ?>
-         <h3><?= $number_of_messages; ?></h3>
-         <p>New messages</p>
-         <a href="messages.php" class="btn">See messages</a>
-      </div>
-
-   </div>
-
-</section>
-
-
-
-
-
-
-
-
-
-
-
-
-<script src="../js/admin_script.js"></script>
-   
+			function disableOther( button ) {
+				if( button !== 'showLeftPush' ) {
+					classie.toggle( showLeftPush, 'disabled' );
+				}
+			}
+		</script>
+	<!--scrolling js-->
+	<script src="js/jquery.nicescroll.js"></script>
+	<script src="js/scripts.js"></script>
+	<!--//scrolling js-->
+	<!-- Bootstrap Core JavaScript -->
+   <script src="js/bootstrap.js"> </script>
 </body>
 </html>
