59e94e038159ae89a4313e6a8b9fd6d29260214e
Subiramaniam Pirashanth <shanth.pirashanth5@gmail.com>
2025-05-19 21:36:27 +0530

    Update invoice-history.php

--- invoice-history.php
+++ invoice-history.php
@@ -1,21 +1,18 @@
-<?php 
+<?php
 session_start();
-error_reporting(0);
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 include('includes/dbconnection.php');
-if (strlen($_SESSION['bpmsuid']==0)) {
-  header('location:logout.php');
-  } else{
 
-
-
-  ?>
+if (empty($_SESSION['bpmsuid'])) {
+    header('location:logout.php');
+    exit();
+}
+?>
 <!doctype html>
 <html lang="en">
   <head>
- 
-
     <title>Beauty Parlour Management System | Invoice History</title>
-
     <!-- Template CSS -->
     <link rel="stylesheet" href="assets/css/style-starter.css">
     <link href="https://fonts.googleapis.com/css?family=Josefin+Slab:400,700,700i&display=swap" rel="stylesheet">
@@ -25,11 +22,8 @@ if (strlen($_SESSION['bpmsuid']==0)) {
   <body id="home">
 <?php include_once('includes/header.php');?>
 
-<script src="assets/js/jquery-3.3.1.min.js"></script> <!-- Common jquery plugin -->
-<!--bootstrap working-->
+<script src="assets/js/jquery-3.3.1.min.js"></script>
 <script src="assets/js/bootstrap.min.js"></script>
-<!-- //bootstrap working-->
-<!-- disable body scroll which navbar is in active -->
 <script>
 $(function () {
   $('.navbar-toggler').click(function () {
@@ -38,16 +32,13 @@ $(function () {
 });
 </script>
 
-<!-- disable body scroll which navbar is in active -->
-
 <!-- breadcrumbs -->
 <section class="w3l-inner-banner-main">
     <div class="about-inner contact ">
         <div class="container">   
             <div class="main-titles-head text-center">
             <h3 class="header-name ">
-                
- Invoice History
+                Invoice History
             </h3>
             <p class="tiltle-para ">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic fuga sit illo modi aut aspernatur tempore laboriosam saepe dolores eveniet.</p>
         </div>
@@ -66,85 +57,86 @@ $(function () {
 </section>
 <!-- breadcrumbs //-->
 <section class="w3l-contact-info-main" id="contact">
-    <div class="contact-sec	">
+    <div class="contact-sec">
         <div class="container">
-
             <div>
                 <div class="cont-details">
                    <div class="table-content table-responsive cart-table-content">
                     <h4 style="padding-bottom: 20px;text-align: center;color: blue;">Invoice History</h4>
                         <table class="table" border="1">
-                            <thead >
+                            <thead>
                                 <tr> 
-                                <th>#</th> 
-                                <th>Invoice Id</th> 
-                                <th>Customer Name</th>
-                                <th>Customer Mobile Number</th>
-                                <th>Invoice Date</th> 
-                                <th>Action</th>
-                            </tr>
+                                    <th>#</th> 
+                                    <th>Invoice Id</th> 
+                                    <th>Customer Name</th>
+                                    <th>Customer Mobile Number</th>
+                                    <th>Invoice Date</th> 
+                                    <th>Action</th>
+                                </tr>
                             </thead>
                             <tbody>
-                               
+<?php
+$userid = $_SESSION['bpmsuid'];
+$stmt = $con->prepare(
+    "SELECT DISTINCT tbluser.ID as uid, tbluser.FirstName, tbluser.LastName, tbluser.Email, tbluser.MobileNumber, tblinvoice.BillingId, DATE(tblinvoice.PostingDate) as PostingDate
+     FROM tbluser
+     JOIN tblinvoice ON tbluser.ID = tblinvoice.Userid
+     WHERE tbluser.ID = ?
+     ORDER BY tblinvoice.ID DESC"
+);
+$stmt->bind_param("s", $userid);
+$stmt->execute();
+$result = $stmt->get_result();
+$cnt = 1;
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+?>
                                 <tr>
-                                    <?php
-                                   $userid= $_SESSION['bpmsuid'];
- $query=mysqli_query($con,"select distinct tbluser.ID as uid, tbluser.FirstName,tbluser.LastName,tbluser.Email,tbluser.MobileNumber,tblinvoice.BillingId,date(tblinvoice.PostingDate) as PostingDate  from  tbluser   
-    join tblinvoice on tbluser.ID=tblinvoice.Userid where tbluser.ID='$userid'order by tblinvoice.ID desc");
-$cnt=1;
-$count=mysqli_num_rows($query);
-if($count>0){
-              while($row=mysqli_fetch_array($query))
-              { ?>
-               <tr> 
-                            <th scope="row"><?php echo $cnt;?></th> 
-                            <td><?php  echo $row['BillingId'];?></td>
-                            <td><?php  echo $row['FirstName'];?> <?php  echo $row['LastName'];?></td>
-                            <td><?php  echo $row['MobileNumber'];?></td>
-                            <td><?php  echo $row['PostingDate'];?></td> 
-                            <td><a href="view-invoice.php?invoiceid=<?php  echo $row['BillingId'];?>" class="btn btn-info">View</a></td> 
-
-                          </tr><?php $cnt=$cnt+1; } } else { ?>
-                            <tr>
-                                <th colspan="6" style="color:red">No Record Found</th>
-                            </tr>
-
-                          <?php } ?>
-                             
+                                    <th scope="row"><?= $cnt; ?></th>
+                                    <td><?= htmlspecialchars($row['BillingId']); ?></td>
+                                    <td><?= htmlspecialchars($row['FirstName'] . ' ' . $row['LastName']); ?></td>
+                                    <td><?= htmlspecialchars($row['MobileNumber']); ?></td>
+                                    <td><?= htmlspecialchars($row['PostingDate']); ?></td>
+                                    <td><a href="view-invoice.php?invoiceid=<?= urlencode($row['BillingId']); ?>" class="btn btn-info">View</a></td>
+                                </tr>
+<?php
+        $cnt++;
+    }
+} else {
+?>
+                                <tr>
+                                    <th colspan="6" style="color:red">No Record Found</th>
+                                </tr>
+<?php } $stmt->close(); ?>
                             </tbody>
                         </table>
-                    </div> </div>
-                
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-   
-    </div></div>
 </section>
 <?php include_once('includes/footer.php');?>
 <!-- move top -->
 <button onclick="topFunction()" id="movetop" title="Go to top">
-	<span class="fa fa-long-arrow-up"></span>
+    <span class="fa fa-long-arrow-up"></span>
 </button>
 <script>
-	// When the user scrolls down 20px from the top of the document, show the button
-	window.onscroll = function () {
-		scrollFunction()
-	};
-
-	function scrollFunction() {
-		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-			document.getElementById("movetop").style.display = "block";
-		} else {
-			document.getElementById("movetop").style.display = "none";
-		}
-	}
-
-	// When the user clicks on the button, scroll to the top of the document
-	function topFunction() {
-		document.body.scrollTop = 0;
-		document.documentElement.scrollTop = 0;
-	}
+window.onscroll = function () {
+    scrollFunction()
+};
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("movetop").style.display = "block";
+    } else {
+        document.getElementById("movetop").style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
 </script>
 <!-- /move top -->
 </body>
-
-</html><?php } ?>
+</html>
