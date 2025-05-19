b0f33ca08751ff85e8729b5e49e44e281d0d4bd4
Subiramaniam Pirashanth <shanth.pirashanth5@gmail.com>
2025-05-19 21:36:09 +0530

    Update change-password.php

--- change-password.php
+++ change-password.php
@@ -1,39 +1,49 @@
-<?php 
+<?php
 session_start();
-error_reporting(0);
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 include('includes/dbconnection.php');
-if (strlen($_SESSION['bpmsuid']==0)) {
-  header('location:logout.php');
-  } else{
-if(isset($_POST['change']))
-{
-$userid=$_SESSION['bpmsuid'];
-$cpassword=md5($_POST['currentpassword']);
-$newpassword=md5($_POST['newpassword']);
-$query1=mysqli_query($con,"select ID from tbluser where ID='$userid' and   Password='$cpassword'");
-$row=mysqli_fetch_array($query1);
-if($row>0){
-$ret=mysqli_query($con,"update tbluser set Password='$newpassword' where ID='$userid'");
 
-echo '<script>alert("Your password successully changed.")</script>';
-} else {
-echo '<script>alert("Your current password is wrong.")</script>';
-
+if (empty($_SESSION['bpmsuid'])) {
+    header('location:logout.php');
+    exit();
 }
 
+if (isset($_POST['change'])) {
+    $userid = $_SESSION['bpmsuid'];
+    $currentpassword = $_POST['currentpassword'] ?? '';
+    $newpassword = $_POST['newpassword'] ?? '';
 
-
+    // Fetch current hashed password
+    $stmt = $con->prepare("SELECT Password FROM tbluser WHERE ID = ?");
+    $stmt->bind_param("s", $userid);
+    $stmt->execute();
+    $stmt->bind_result($hashedPassword);
+    if ($stmt->fetch()) {
+        if (password_verify($currentpassword, $hashedPassword)) {
+            $stmt->close();
+            $newHashed = password_hash($newpassword, PASSWORD_DEFAULT);
+            $update = $con->prepare("UPDATE tbluser SET Password = ? WHERE ID = ?");
+            $update->bind_param("ss", $newHashed, $userid);
+            if ($update->execute()) {
+                echo '<script>alert("Your password successfully changed.")</script>';
+            } else {
+                echo '<script>alert("Something went wrong. Please try again.")</script>';
+            }
+            $update->close();
+        } else {
+            echo '<script>alert("Your current password is wrong.")</script>';
+        }
+    } else {
+        echo '<script>alert("User not found.")</script>';
+    }
+    $stmt->close();
 }
-
-
-  ?>
+?>
 <!doctype html>
 <html lang="en">
   <head>
- 
-
-    <title>Beauty Parlour Management System | Signup Page</title>
-
+    <title>Beauty Parlour Management System | Change Password</title>
     <!-- Template CSS -->
     <link rel="stylesheet" href="assets/css/style-starter.css">
     <link href="https://fonts.googleapis.com/css?family=Josefin+Slab:400,700,700i&display=swap" rel="stylesheet">
@@ -43,32 +53,23 @@ echo '<script>alert("Your current password is wrong.")</script>';
   <body id="home">
 <?php include_once('includes/header.php');?>
 
-<script src="assets/js/jquery-3.3.1.min.js"></script> <!-- Common jquery plugin -->
-<!--bootstrap working-->
+<script src="assets/js/jquery-3.3.1.min.js"></script>
 <script src="assets/js/bootstrap.min.js"></script>
-<!-- //bootstrap working-->
-<!-- disable body scroll which navbar is in active -->
 <script>
 $(function () {
   $('.navbar-toggler').click(function () {
     $('body').toggleClass('noscroll');
   })
 });
-</script>
-<script type="text/javascript">
-function checkpass()
-{
-if(document.changepassword.newpassword.value!=document.changepassword.confirmpassword.value)
-{
-alert('New Password and Confirm Password field does not match');
-document.changepassword.confirmpassword.focus();
-return false;
+function checkpass() {
+    if(document.changepassword.newpassword.value != document.changepassword.confirmpassword.value) {
+        alert('New Password and Confirm Password field does not match');
+        document.changepassword.confirmpassword.focus();
+        return false;
+    }
+    return true;
 }
-return true;
-} 
-
 </script>
-<!-- disable body scroll which navbar is in active -->
 
 <!-- breadcrumbs -->
 <section class="w3l-inner-banner-main">
@@ -76,8 +77,7 @@ return true;
         <div class="container">   
             <div class="main-titles-head text-center">
             <h3 class="header-name ">
-                
- Change Password
+                Change Password
             </h3>
             <p class="tiltle-para ">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic fuga sit illo modi aut aspernatur tempore laboriosam saepe dolores eveniet.</p>
         </div>
@@ -96,25 +96,21 @@ return true;
 </section>
 <!-- breadcrumbs //-->
 <section class="w3l-contact-info-main" id="contact">
-    <div class="contact-sec	">
+    <div class="contact-sec">
         <div class="container">
-
             <div class="d-grid contact-view">
                 <div class="cont-details">
                     <?php
-
-$ret=mysqli_query($con,"select * from tblpage where PageType='contactus' ");
-$cnt=1;
-while ($row=mysqli_fetch_array($ret)) {
-
-?>
+                    $ret = mysqli_query($con, "SELECT * FROM tblpage WHERE PageType='contactus'");
+                    while ($row = mysqli_fetch_array($ret)) {
+                    ?>
                     <div class="cont-top">
                         <div class="cont-left text-center">
                             <span class="fa fa-phone text-primary"></span>
                         </div>
                         <div class="cont-right">
                             <h6>Call Us</h6>
-                            <p class="para"><a href="tel:+44 99 555 42">+<?php  echo $row['MobileNumber'];?></a></p>
+                            <p class="para"><a href="tel:+44 99 555 42">+<?php echo htmlspecialchars($row['MobileNumber']); ?></a></p>
                         </div>
                     </div>
                     <div class="cont-top margin-up">
@@ -123,7 +119,7 @@ while ($row=mysqli_fetch_array($ret)) {
                         </div>
                         <div class="cont-right">
                             <h6>Email Us</h6>
-                            <p class="para"><a href="mailto:example@mail.com" class="mail"><?php  echo $row['Email'];?></a></p>
+                            <p class="para"><a href="mailto:example@mail.com" class="mail"><?php echo htmlspecialchars($row['Email']); ?></a></p>
                         </div>
                     </div>
                     <div class="cont-top margin-up">
@@ -132,7 +128,7 @@ while ($row=mysqli_fetch_array($ret)) {
                         </div>
                         <div class="cont-right">
                             <h6>Address</h6>
-                            <p class="para"> <?php  echo $row['PageDescription'];?></p>
+                            <p class="para"><?php echo htmlspecialchars($row['PageDescription']); ?></p>
                         </div>
                     </div>
                     <div class="cont-top margin-up">
@@ -141,59 +137,52 @@ while ($row=mysqli_fetch_array($ret)) {
                         </div>
                         <div class="cont-right">
                             <h6>Time</h6>
-                            <p class="para"> <?php  echo $row['Timing'];?></p>
+                            <p class="para"><?php echo htmlspecialchars($row['Timing']); ?></p>
                         </div>
                     </div>
-               <?php } ?> </div>
+                    <?php } ?>
+                </div>
                 <div class="map-content-9 mt-lg-0 mt-4">
                     <h3>Password change!!</h3>
                     <form method="post" name="changepassword" onsubmit="return checkpass();">
-
                         <div style="padding-top: 30px;">
                             <label>Current Password</label>
-                            
-                            <input type="password" class="form-control" placeholder="Current Password" id="currentpassword" name="currentpassword" value="" required="true"></div>
-                           <div style="padding-top: 30px;">
+                            <input type="password" class="form-control" placeholder="Current Password" id="currentpassword" name="currentpassword" required>
+                        </div>
+                        <div style="padding-top: 30px;">
                             <label>New Password</label>
-                            
-                            <input type="password" class="form-control" placeholder="New Password" id="newpassword" name="newpassword" value="" required="true">
+                            <input type="password" class="form-control" placeholder="New Password" id="newpassword" name="newpassword" required>
                         </div>
                         <div style="padding-top: 30px;">
                             <label>Confirm Password</label>
-                           <input type="password" class="form-control" placeholder="Confirm Password" id="confirmpassword" name="confirmpassword" value=""  required="true">
+                            <input type="password" class="form-control" placeholder="Confirm Password" id="confirmpassword" name="confirmpassword" required>
+                        </div>
                         <button type="submit" class="btn btn-contact" name="change">Save Change</button>
                     </form>
                 </div>
+            </div>
+        </div>
     </div>
-   
-    </div></div>
 </section>
 <?php include_once('includes/footer.php');?>
 <!-- move top -->
 <button onclick="topFunction()" id="movetop" title="Go to top">
-	<span class="fa fa-long-arrow-up"></span>
+    <span class="fa fa-long-arrow-up"></span>
 </button>
 <script>
-	// When the user scrolls down 20px from the top of the document, show the button
-	window.onscroll = function () {
-		scrollFunction()
-	};
-
-	function scrollFunction() {
-		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-			document.getElementById("movetop").style.display = "block";
-		} else {
-			document.getElementById("movetop").style.display = "none";
-		}
-	}
-
-	// When the user clicks on the button, scroll to the top of the document
-	function topFunction() {
-		document.body.scrollTop = 0;
-		document.documentElement.scrollTop = 0;
-	}
+window.onscroll = function () { scrollFunction(); };
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("movetop").style.display = "block";
+    } else {
+        document.getElementById("movetop").style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
 </script>
 <!-- /move top -->
 </body>
-
-</html><?php } ?>
+</html>
